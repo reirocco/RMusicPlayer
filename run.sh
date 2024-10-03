1eaf9a9c617f46301d165964abdc45c8384384bb
Rocco Nori <norirocco11@gmail.com>
2024-10-03 16:03:11 +0200

    adding automation script

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Nome del virtual environment (modifica a piacere)
+venv_name=enviroment
+pwd
+# Controlla se il virtual environment esiste già
+if [ ! -d "$venv_name" ]; then
+  # Crea il virtual environment
+  python3 -m venv $venv_name
+  echo "Virtual environment creato: $venv_name"
+fi
+
+# Attiva il virtual environment
+source $venv_name/bin/activate
+
+# Controlla se esiste il file requirements.txt
+if [ -f requirements.txt ]; then
+  # Installa i pacchetti dal file requirements.txt
+  pip install -r requirements.txt
+  echo "Pacchetti installati da requirements.txt"
+else
+  echo "File requirements.txt non trovato."
+fi
+
+# Crea una nuova sessione screen e avvia Flask al suo interno
+screen -S RMusicPlayer -dm bash -c 'flask run'
